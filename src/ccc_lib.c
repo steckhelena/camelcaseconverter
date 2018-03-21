640d73a21fa345538de16b6bf3e8471e1de0eeb7
Helena Steck <steckhelena@gmail.com>
2018-03-20 23:25:11 -0300

    Finished lab, just missing some documentation.

--- src/ccc_lib.c
+++ src/ccc_lib.c
@@ -1,14 +1,34 @@
 
-#include "ccclib.h"
+#include "ccc_lib.h"
 
 void camelcase_to_underline(char *input, char *output) {
+	*(output++) = *(input++) + 32;
+	while (*input != '\0') {
+		if (*input >= 'A' && *input <= 'Z') {
+			*(output++) = '_';
+			*(output++) = *(input++) + 32;
+		} else {
+			*(output++) = *(input++);
+		}
+	}
 
-  return;
+	return;
 }
 
 void underline_to_camelcase(char *input, char *output) {
+	if (*input != '_') {
+		*(output++) = *(input++) - 32;
+	}
+	while (*input != '\0') {
+		if (*input == '_') {
+			*(output++) = *(++input) - 32;
+			input++;
+		} else {
+			*(output++) = *(input++);
+		}
+	}
 
-  return;
+	return;
 }
 
 
